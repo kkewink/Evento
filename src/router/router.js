5008831e352109af3d6b97d6b5ade8ac2becb44c
Eduardo B. <146993235+kkewink@users.noreply.github.com>
2024-09-23 16:42:43 -0300

    ...

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,11 @@
 const {Router} = require("express");
 const RouterE = require("./routerEvent");
-
+const RouterP = require("./routerPartic");
 
 const router = Router();
 router.use('/event', RouterE);
+
+router.use('/participante', RouterP);
 
 
 module.exports = router;
---
+++ src/router/routerPartic.js
@@ -0,0 +1,27 @@
+const {Router} = require("express");
+const ParticipanteController = require("../controller/ParticipanteController");
+const { route } = require("./routerEvent");
+
+const router = Router();
+
+router.post('/', (req,res) =>{
+    ParticipanteController.create(req,res);
+})
+
+router.get('/' , (req,res) =>{
+    ParticipanteController.getAll(req,res);
+});
+
+router.get('/:id', (req,res) =>{
+    ParticipanteController.getOne(req,res);
+});
+
+router.put('/:id', (req,res) =>{
+    ParticipanteController.update(req,res);
+});
+
+router.delete('/:id', (req,res)=>{
+    ParticipanteController.delete(req,res);
+});
+
+module.exports = route;
